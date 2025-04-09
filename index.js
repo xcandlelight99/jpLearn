f131f0a45743bd7857bbd70321472f4b66dec373
xcandlelight99 <132201907+xcandlelight99@users.noreply.github.com>
2025-04-09 21:35:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,19 +5,32 @@ function displayName(){
     for (let x in dataAll){
         const para = document.createElement("li");
         para.innerHTML = 
-        '<h1>' + dataAll[x].kanji + '</h1>' +
-        '<span id="detail">'+ dataAll[x].description +'</span>';
-        para.id = "kanji" + dataAll[x].id;
+        '<div>' + '<h1>' + dataAll[x].kanji + '</h1>' + '</div>' +
+        '<div id="hidden">' + 
+        '<span id="kunSound">' + dataAll[x].kun + '</span>' +
+        '<span>' + '・' + '</span>' +
+        '<span id="onSound">' + dataAll[x].on + '</span>' +
+        '</div>' +
+        '<div id="hidden">' + 
+        '<span>' + dataAll[x].meaning + '</span>' +
+        '<span>' + '・' + '</span>'  + 
+        '<span>' + dataAll[x].description + '</span>' +
+        '</div>';
         para.className = "level" + dataAll[x].level;
         para.onclick = function(){
-            const detail = para.querySelector('#detail');
-            if (detail.style.display === "none") {
+            const oneHidden = para.querySelector('#hidden');
+            const allHidden = para.querySelectorAll('#hidden');
+            if (oneHidden.style.display === "none") {
                 para.style.gridColumn = 'span 8';
-                detail.style.display = "block";
+                allHidden.forEach(element => {
+                    element.style.display = "block";
+                });
             } 
             else{
                 para.style.gridColumn = 'span 1';
-                detail.style.display = "none";
+                allHidden.forEach(element => {
+                    element.style.display = "none";
+                });
             }
         }
         document.getElementById("contentList").appendChild(para);
