5dc347c68f6706c197c5f45925c67a7864cb946a
xcandlelight99 <132201907+xcandlelight99@users.noreply.github.com>
2025-04-13 22:52:28 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -6,24 +6,9 @@ function displayName(){
         const para = document.createElement("li");
         para.innerHTML = 
         '<div>' + '<h1>' + dataAll[x].kanji + '</h1>' + '</div>' +
-        '<div id="hidden">' + dataAll[x].reading + '</div>' +
-        '<div id="hidden">' + dataAll[x].meaning + '</div>';
-        para.onclick = function(){
-            const oneHidden = para.querySelector('#hidden');
-            const allHidden = para.querySelectorAll('#hidden');
-            if (oneHidden.style.display === "none") {
-                para.style.gridColumn = 'span 8';
-                allHidden.forEach(element => {
-                    element.style.display = "block";
-                });
-            } 
-            else{
-                para.style.gridColumn = 'span 1';
-                allHidden.forEach(element => {
-                    element.style.display = "none";
-                });
-            }
-        }
+        '<div>' + dataAll[x].reading + '</div>' +
+        '<div>' + dataAll[x].meaning + '</div>';
+        '<div>' + dataAll[x].description + '</div>';
         document.getElementById("contentList").appendChild(para);
     }
 };
